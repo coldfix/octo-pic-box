9c0a877fd2b554c9779ceb7c5bac7aec1e6b3aae
Thomas Gläßle <t_glaessle@gmx.de>
2017-08-07 12:43:35 +0200

    Allow large file uploads (100MB)

--- init.sh
+++ init.sh
@@ -6,6 +6,9 @@ if [[ $HTTP_AUTH = digest ]]; then
     <apache2_auth.conf >>/etc/apache2/sites-enabled/000-default.conf
 fi
 
+sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 100M/' /etc/php5/apache2/php.ini
+sed -i 's/post_max_size = 8M/post_max_size = 100M/' /etc/php5/apache2/php.ini
+
 #mkdir -p /picbox/files/upload
 mkdir -p /picbox/thumbs
 mkdir -p /picbox/highlights
